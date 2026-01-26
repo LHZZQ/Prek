621745f3891bc1085f2dbe72315ee9702ef625ee
carol <pn24594@bristol.ac.uk>
2026-01-26 13:02:02 +0000

    changed appbar to back button

--- prek-app/lib/settings_page.dart
+++ prek-app/lib/settings_page.dart
@@ -1,52 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:_2025_prek/pages/entry_history_page.dart';
-import 'package:_2025_prek/profile_page.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const pink = Color(0xFFFFC7E0);
-    const peach = Color(0xFFFFE4B5);
-    const softWhite = Color(0xFFFFFFFF);
     const textColor = Color(0xFF94697E);
+    const topBarColor = Color(0xFFFFF1F5);
 
     return Scaffold(
+      backgroundColor: topBarColor,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        title: const Text('Settings'),
+        backgroundColor: topBarColor,
         elevation: 0,
-        leading: PopupMenuButton<String>(
-          icon: const Icon(Icons.menu, color: textColor, size: 30),
-          color: Colors.white,
-          onSelected: (value) {
-            if (value == 'profile') {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ProfilePage()),
-              );
-            } else if (value == 'history') {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const EntryHistoryPage(),
-                ),
-              );
-            } else if (value == 'settings') {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SettingsPage()),
-              );
-            }
-          },
-
-          itemBuilder: (context) {
-            return const [
-              PopupMenuItem(value: 'profile', child: Text('Profile')),
-              PopupMenuItem(value: 'settings', child: Text('Settings')),
-              PopupMenuItem(value: 'history', child: Text('History')),
-            ];
-          },
+        foregroundColor: textColor,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFFFF1F5), Color(0xFFFFF8EE)],
+          ),
         ),
       ),
     );
