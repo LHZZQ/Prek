aa056678b8a8a108c33d23f5ac0ab1bd8e1fdf88
carol <pn24594@bristol.ac.uk>
2026-01-28 20:26:47 +0000

    finished change name page

--- prek-app/lib/change_name.dart
+++ prek-app/lib/change_name.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ChangeName extends StatelessWidget {
+class ChangeName extends StatefulWidget {
   const ChangeName({super.key});
+  @override
+  State<ChangeName> createState() => _ChangeNameState();
+}
+
+class _ChangeNameState extends State<ChangeName> {
+  final TextEditingController nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +37,75 @@ class ChangeName extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 25),
-            child: SingleChildScrollView(child: Column(children: [
-                  ],
-              )),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  //logo
+                  Image(image: AssetImage('images/prek_logo.png')),
+
+                  //new name
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.pinkAccent.withValues(alpha: 0.1),
+                          blurRadius: 6,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: nameController,
+
+                      decoration: const InputDecoration(
+                        hintText: "Enter your new name",
+                        contentPadding: EdgeInsets.all(20),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 50),
+                  //save button
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      gradient: const LinearGradient(
+                        colors: [
+                          Color.fromRGBO(255, 199, 224, 1),
+                          Color.fromRGBO(255, 228, 181, 1),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                    ),
+
+                    child: ElevatedButton(
+                      onPressed: () {},
+
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        shadowColor: Colors.transparent,
+                        minimumSize: const Size(double.infinity, 55),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                      child: const Text(
+                        "Save",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
