b99487f1253326babaeaf026ce3f1b593ca7e2cf
layanalajeel7 <pk23085@bristol.ac.uk>
2026-01-29 12:34:15 +0000

    more colors added/ switched

--- prek-app/lib/profile_page.dart
+++ prek-app/lib/profile_page.dart
@@ -5,10 +5,11 @@ class ProfilePage extends StatelessWidget{
 
   @override 
   Widget build(BuildContext context){
-    const pink = Color(0xFFFFC7E0);
-    const peach = Color(0xFFFFE4B5);
-    const softWhite = Color(0xFFFFFFFF);
-    const textColor = Color(0xFF94697E);
+    const pink = Color(0xFFFB7DA8);
+    const yellow = Color(0xFFFFC567);
+    const retroBlue = Color(0xFF058CD7);
+    const softWhite = Color(0xFFFFFFFF); 
+    const textColor = Color(0xFF94697E); 
     const bgTop = Color(0xFFFFF1F5);
     const bgBottom = Color(0xFFFFF8EE);
     
@@ -19,6 +20,10 @@ class ProfilePage extends StatelessWidget{
         elevation: 0,
         iconTheme: const IconThemeData(color: textColor),
         centerTitle: true,
+        leading: IconButton(icon: const
+        Icon(Icons.arrow_back_ios_new_rounded, color: textColor),
+        onPressed: () => Navigator.pop(context),
+        ),
         title: const Text(
           "Profile",
           style: TextStyle(
@@ -30,7 +35,7 @@ class ProfilePage extends StatelessWidget{
           IconButton(
             onPressed: () {
               ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text("Action not available")),
+                const SnackBar(content: Text("not available right now")),
               );
             },
             icon: const Icon(Icons.logout_rounded, color: textColor),
