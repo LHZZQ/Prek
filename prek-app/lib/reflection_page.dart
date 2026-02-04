64cde59a547ecdb89642b78c6ad34eef759ff2c8
DaudIsmail1 <kk24104@bristol.ac.uk>
2026-02-04 16:30:57 +0000

    Initial draft, shows text in history however does not show emoji

--- prek-app/lib/reflection_page.dart
+++ prek-app/lib/reflection_page.dart
@@ -1,25 +1,23 @@
+import 'package:_2025_prek/home_page.dart';
 import 'package:_2025_prek/services/gratitude_service.dart';
 import 'package:flutter/material.dart';
 
-
-class ReflectionPage extends StatefulWidget { 
+class ReflectionPage extends StatefulWidget {
   final String selectedMood;
-  const ReflectionPage({
-  super.key, 
-  required this.selectedMood,});
- 
-  @override 
-  State<ReflectionPage> createState() => _ReflectionPageState(); 
-} 
- 
-class _ReflectionPageState extends State<ReflectionPage> { 
+  const ReflectionPage({super.key, required this.selectedMood});
+
+  @override
+  State<ReflectionPage> createState() => _ReflectionPageState();
+}
+
+class _ReflectionPageState extends State<ReflectionPage> {
   final _controller = TextEditingController(); //text controller for inputs
- 
-  @override 
+
+  @override
   Widget build(BuildContext context) {
-    //colors 
+    //colors
     const pink = Color(0xFFFFC7E0);
-   // const peach = Color(0xFFFFE4B5);
+    // const peach = Color(0xFFFFE4B5);
     const textColor = Color(0xFF94697E);
 
     return Scaffold(
@@ -37,7 +35,6 @@ class _ReflectionPageState extends State<ReflectionPage> {
       ),
 
       body: Container(
-
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             // background gradient
@@ -46,133 +43,151 @@ class _ReflectionPageState extends State<ReflectionPage> {
             colors: [Color(0xFFFFF1F5), Color(0xFFFFF8EE)],
           ),
         ),
-      child: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(minHeight: constraints.maxHeight),
-              child: Center(
+        child: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 820),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: Center(
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 820),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
 
-          
-          children: [
-            const SizedBox(height: 30),
-            const Text(
-              "Take a moment to reflect on something you're grateful for today 💭", //header text
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: textColor,
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                height: 1.5,
-              ),
-            ),
-            
+                        children: [
+                          const SizedBox(height: 30),
+                          const Text(
+                            "Take a moment to reflect on something you're grateful for today 💭", //header text
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: textColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              height: 1.5,
+                            ),
+                          ),
 
-            const SizedBox(height: 40),
+                          const SizedBox(height: 40),
 
-            Container(
-              decoration: BoxDecoration(
-                //input box
-                color: Colors.white70,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.pinkAccent.withValues(alpha: 0.1),
-                    blurRadius: 6,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: TextField(
-                controller: _controller,
-                maxLines: 9, //enough space for a short reflection
-                decoration: const InputDecoration(
-                  hintText: "Write your reflection here...",
-                  contentPadding: EdgeInsets.all(20),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
+                          Container(
+                            decoration: BoxDecoration(
+                              //input box
+                              color: Colors.white70,
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.pinkAccent.withValues(
+                                    alpha: 0.1,
+                                  ),
+                                  blurRadius: 6,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: TextField(
+                              controller: _controller,
+                              maxLines: 9, //enough space for a short reflection
+                              decoration: const InputDecoration(
+                                hintText: "Write your reflection here...",
+                                contentPadding: EdgeInsets.all(20),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
 
-            const SizedBox(height: 40),
+                          const SizedBox(height: 40),
 
-            Container(
-              width: double.infinity,
-              height: 62,
-              //gradient wrapper for button
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                gradient: const LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [
-                    Color(0xFFFFC567), // yellow
-                    Color(0xFFFB7DA8), // pink
-                    Color(0xFF058CD7), // blue
-                  ],
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.pinkAccent.withValues(alpha: 0.25),
-                    blurRadius: 15,
-                    offset: const Offset(0, 6),
-                  ),
-                ],
-              ),
+                          Container(
+                            width: double.infinity,
+                            height: 62,
+                            //gradient wrapper for button
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              gradient: const LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                  Color(0xFFFFC567), // yellow
+                                  Color(0xFFFB7DA8), // pink
+                                  Color(0xFF058CD7), // blue
+                                ],
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.pinkAccent.withValues(
+                                    alpha: 0.25,
+                                  ),
+                                  blurRadius: 15,
+                                  offset: const Offset(0, 6),
+                                ),
+                              ],
+                            ),
 
-              child: ElevatedButton(
-                //save button
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                  padding: EdgeInsets.zero,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                ),
+                            child: ElevatedButton(
+                              //save button
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.transparent,
+                                shadowColor: Colors.transparent,
+                                padding: EdgeInsets.zero,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                              ),
 
-                onPressed: () async {
-                  final text = _controller.text.trim();
+                              onPressed: () async {
+                                final text = _controller.text.trim();
 
-                  if (text.isEmpty) return;
+                                if (text.isEmpty) return;
 
-                  try {
-                    await saveGratitudeEntry(text);
-                    
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          "🌸 Reflection saved (will link to history later)!",
-                        ),
-                        backgroundColor: pink.withValues(alpha: 0.9),
-                      ),
-                    );
+                                try {
+                                  await saveGratitudeEntry(
+                                    text: text,
+                                    mood: widget.selectedMood,
+                                  );
 
-                    _controller.clear(); //clears input
-                  } catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("Error saving reflection: $e"),
-                      backgroundColor: Colors.redAccent,
-                      )
-                    );
-                  }
-                }, 
+                                  if (!mounted) return;
 
-                child: const Text(
-                  "Save Reflection",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    ),
+                                  Navigator.pushAndRemoveUntil(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) => const HomePage(),
+                                    ),
+                                    (route) => false,
+                                  );
+
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                        "🌸 Reflection saved (will link to history later)!",
+                                      ),
+                                      backgroundColor: pink.withValues(
+                                        alpha: 0.9,
+                                      ),
+                                    ),
+                                  );
+
+                                  _controller.clear(); //clears input
+                                } catch (e) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                        "Error saving reflection: $e",
+                                      ),
+                                      backgroundColor: Colors.redAccent,
+                                    ),
+                                  );
+                                }
+                              },
+
+                              child: const Text(
+                                "Save Reflection",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
                               ),
                             ),
                           ),
