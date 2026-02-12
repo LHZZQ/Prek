e0f488acabdc3dfa04600524fe2f85b6e64e9a78
layanalajeel7 <pk23085@bristol.ac.uk>
2026-02-12 14:58:05 +0000

    intializing page

---
+++ prek-app/lib/picture_reflection_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PictureReflectionPage extends StatelessWidget {
+  const PictureReflectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //const pink = Color(0xFFFB7DA8);
+    //const yellow = Color(0xFFFFC567);
+    //const blue = Color(0xFF058CD7);
+    //const softWhite = Color(0xFFFFFFFF);
+    //const textColor = Color(0xFF94697E);
+    //const bgTop = Color(0xFFFFF1F5);
+    //const bgBottom = Color(0xFFFFF8EE);
+
+  }
+
+}
