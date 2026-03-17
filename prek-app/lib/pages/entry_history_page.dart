0be05589f2382df097548852ac189436e5c03af7
DaudIsmail1 <kk24104@bristol.ac.uk>
2026-03-17 16:35:36 +0000

    Integrated into the history page

--- prek-app/lib/pages/entry_history_page.dart
+++ prek-app/lib/pages/entry_history_page.dart
@@ -112,7 +112,14 @@ class _EntryHistoryPageState extends State<EntryHistoryPage> {
                       ),
                     ),
                   ),
-                GratitudeTile(entry: e),
+                GratitudeTile(
+                  entry: e,
+                  onDeleted: () {
+                    setState(() {
+                      items.removeWhere((item) => item.id == e.id );
+                    });
+                  },
+                  ),
               ],
             );
           },
