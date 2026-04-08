c27fa9441d8cc166afb28a3d30f54c9a2d4d0074
carol <pn24594@bristol.ac.uk>
2026-04-08 16:03:55 +0100

    updated theme

--- prek-app/lib/theme/theme.dart
+++ prek-app/lib/theme/theme.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
-  colorScheme: ColorScheme.light(background: Colors.black),
+  colorScheme: const ColorScheme.light(
+    background: Color(0xFFFFF1F5),
+    primary: Color(0xFFFFF8EE),
+  ),
 );
 
 ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
-    background: Colors.grey.shade900,
-    primary: Colors.grey.shade800,
-    secondary: Colors.grey.shade700,
+    background: Color(0xFF1E1E2C),
+    primary: Color(0xFF2A2A3D),
   ),
 );
