29692c6d6a5818ab3a1ca9487b5fd7c2f988e601
LHZZQ <zhangziqian616@163.com>
2026-04-14 11:48:20 +0100

    add password vali test

--- prek-app/test/signup_test.dart
+++ prek-app/test/signup_test.dart
@@ -5,6 +5,11 @@ void main() {
   final validator = SignUpValidator();
 
   group('SignUpValidator Logic', () {
+    test('validateEmail returns error when email is null', () {
+      var result = validator.validateEmail(null);
+      expect(result, 'Email is required');
+    });
+
     test('validateEmail returns error when email is empty', () {
       var result = validator.validateEmail('');
       expect(result, 'Email is required');
@@ -18,6 +23,16 @@ void main() {
     test('validateEmail returns null when email is valid', () {
       var result = validator.validateEmail('test@gmail.com');
       expect(result, null);
+    });
+
+    test('validateEmail trims whitespace and still validates', () {
+      var result = validator.validateEmail('  test@gmail.com  ');
+      expect(result, null);
+    });
+
+    test('validatePassword returns error when password is null', () {
+      var result = validator.validatePassword(null);
+      expect(result, 'Password is required');
     });
 
     test('validatePassword returns error when password is empty', () {
@@ -36,6 +51,14 @@ void main() {
     test('validatePassword returns null when valid', () {
       var result = validator.validatePassword('Kitty@5689');
       expect(result, null);
+    });
+
+    test('validatePassword returns error when do not haveuppercase', () {
+      var result = validator.validatePassword('zzq@56890');
+      expect(
+        result,
+        'Password must have a minimum of 1 lower case letter [a-z], a minimum of 1 upper case letter [A-Z], a minimum of 1 numeric character [0-9], a minimum of 1 special character: ~`!@#%^&*()-_+={}[]|:"<>,./?, and must be at least 10 characters',
+      );
     });
   });
 }
