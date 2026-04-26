6ef6c9ab4e34f70ea28d27f8f7ec543f6fd37dfe
DaudIsmail1 <kk24104@bristol.ac.uk>
2026-04-26 22:16:40 +0100

    Formatted after bug

--- prek-app/lib/widgets/gratitude_tile.dart
+++ prek-app/lib/widgets/gratitude_tile.dart
@@ -141,7 +141,7 @@ class _GratitudeTileState extends State<GratitudeTile> {
           if (s == PlayerState.completed && _isMine) {
             //complete and was mine
             _currentSrc = null;
-            _dur = Duration.zero
+            _dur = Duration.zero;
           }
         }
       });
